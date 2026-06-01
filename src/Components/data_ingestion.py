4ed23b2780b254cfb901860da214099e07c03785
offgridmode <aslaunch777@gmail.com>
2026-06-01 11:38:01 +0530

    Completed DataTransformation.py

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -1,52 +1,90 @@
-## this files helps fetching/reading data from different data sources
-##all the code here will help reading data only and act as a major component to the project
-
-
-import os 
-import sys 
+import os
+import sys
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.Components.data_transformation import DataTransformationConfig
+from src.Components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts', "train.csv")
-    test_data_path: str=os.path.join('artifacts', "test.csv")  ### generally we use decorator on a class for defining clss variables 
-    raw_data_path: str=os.path.join('artifacts', "data.csv")
-    
-class DataIngestion:  ## for a class containing fuction we should with self init parameter only.
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
+
+
+class DataIngestion:
+
     def __init__(self):
-        self.ingestion_config= DataIngestionConfig() ## this class variable contains the above class variable inside it
-    
+        self.ingestion_config = DataIngestionConfig()
+
     def initiate_data_ingestion(self):
+
         logging.info("Entered the Data Ingestion Method or Component")
+
         try:
-            df= pd.read_csv('NoteBook\Data\StudentsPerformance.csv') #### here we can provide any path to read our data from any source like dbs,etc.
+
+            df = pd.read_csv(
+                r'NoteBook\Data\StudentsPerformance.csv'
+            )
+
             logging.info("Read the dataset as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(
+                os.path.dirname(
+                    self.ingestion_config.train_data_path
+                ),
+                exist_ok=True
+            )
 
-            df.to_csv(self.ingestion_config.raw_data_path, index= False, header= True )
+            df.to_csv(
+                self.ingestion_config.raw_data_path,
+                index=False,
+                header=True
+            )
+
             logging.info("Train test split initiated")
-            
 
+            train_set, test_set = train_test_split(
+                df,
+                test_size=0.2,
+                random_state=42
+            )
 
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False, header= True)
+            train_set.to_csv(
+                self.ingestion_config.train_data_path,
+                index=False,
+                header=True
+            )
 
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False, header= True)
+            test_set.to_csv(
+                self.ingestion_config.test_data_path,
+                index=False,
+                header=True
+            )
 
             logging.info("Ingestion of the data is completed")
-            return(
+
+            return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
             )
+
         except Exception as e:
-            raise CustomException(e,sys)
-if __name__=="__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
- 
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+
+    obj = DataIngestion()
+
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    data_transformation.initiate_data_transformation(
+        train_data,
+        test_data
+    )
